8f7cfff308fe7d8c43082cc70cdee4991cc76831
Rodrigo Junior <rodrigo59651736@edu.pa.senac.br>
2026-03-23 20:47:43 -0300

    Aula dia 23/03

---
+++ tipos.py
@@ -0,0 +1,27 @@
+#Variaveis
+var = "João"
+print(var)
+var = "José"
+print(var)
+var = 15
+print(var)
+var = "15"
+print(var)
+
+
+#Tipos de dados
+#Inteiro (Int)
+Inteiro = 1
+#Fracionário (Float)
+Fracionário = 1.0
+#Textos (String)
+Textos = "Exemplo"
+#Lógico (Boolean)
+Lógico = True
+#Vazio
+Vazio = None
+
+print(5%2)
+
+
+#Constantes
