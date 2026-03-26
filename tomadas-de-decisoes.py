cf00bcbf3a36ba673944de586dd1a6c611dd80fc
Rodrigo Junior <rodrigo59651736@edu.pa.senac.br>
2026-03-26 20:59:30 -0300

    Tomadas de decisões 26/03

---
+++ tomadas-de-decisoes.py
@@ -0,0 +1,29 @@
+var_int = 5
+var_float = 1.0
+var_str = "Texto"
+var_bool = True
+
+# Operações Aritmética
+
+var_int = 1+1
+
+# Operação relacional
+# Igual
+var_bool_1 = 3 == 3  
+# Desigual  
+var_bool_2 = 3 != 9
+# Menor
+var_bool_3 = 9 < 5
+# Maior
+var_bool_4 = 5 > 1
+# Maior ou Igual
+var_bool_5 = 2 >= 2
+# Menor ou Igual
+var_bool_6 = 5 <= 5
+print(var_bool_6)
+
+teste = True == 3-2
+print(teste)
+
+# True = 1
+# False = 0
