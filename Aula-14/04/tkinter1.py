444bd6f07c20ea66c003cef2b503fee8fe837a0c
Rodrigo Junior <rodrigo59651736@edu.pa.senac.br>
2026-04-14 20:57:33 -0300

    Tkinter

---
+++ Aula-14/04/tkinter1.py
@@ -0,0 +1,24 @@
+import tkinter as tk
+
+janela = tk.Tk()
+janela.geometry('400x200')
+
+frame1 = tk.Frame(janela, bg='lightblue', width=100, height=200, bd=1)
+frame1.place(x=0, y=0)
+
+frame2 = tk.Frame(janela, bg='lightgreen', width=300, height=150, bd=2)
+frame2.place(x=100, y=0)
+
+frame3 = tk.Frame(janela, bg='yellow', width=400, height=50, bd=3)
+frame3.place(x=100, y=150)
+
+label1 = tk.Label(frame1, text="Frame Esquerdo", bg='lightblue')
+label1.place(x=0, y=0)
+
+label2 = tk.Label(frame2, text="Frame Direito", bg='lightgreen')
+label2.place(x=0, y=0)
+
+label3 = tk.Label(frame3, text="Frame Inferior Direito", bg='yellow')
+label3.place(x=0, y=0)
+
+janela.mainloop()
